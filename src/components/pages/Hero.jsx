93c1c57efa95043ce2f2896ee6e3c2ff759f3b5b
rsayyed591 <rehansayyed591@gmail.com>
2025-01-06 16:37:44 +0530

    updated hero

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -1,13 +1,21 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useEffect } from 'react';
 import heroimg from './../../assets/hero.png';
 import './../../css/style.css';
-import { Link } from 'react-router-dom'
-// import {Counter,Services,Features,Try,About} from "./";
-import {Counter,Services,Features,Try, Footer, Usecase, Architecture, Objectives} from "./";
-
+import { Link } from 'react-router-dom';
+import { Counter, Services, Features, Try, Footer, Usecase, Architecture, Objectives } from "./";
 
 const Hero = () => {
+  // Alert when the component is loaded
+  useEffect(() => {
+    const userResponse = window.confirm(
+      "This website has been designed using Next.js. Would you like to proceed to the Next.js version hosted at https://ishaaraai.vercel.app/?"
+    );
+    if (userResponse) {
+      window.location.href = "https://ishaaraai.vercel.app/";
+    }
+  }, []);
+
   return (
     <section
       id="hero"
@@ -15,14 +23,12 @@ const Hero = () => {
         justifyContent: 'center',
         alignItems: 'center',
         maxHeight: '80%',
-        // marginBottom: '21vh',
-        background:'white'
+        background: 'white',
       }}
     >
-      <div style={{ padding: '3.5%', marginBottom:'10%'}}>
-        <div className='wrapper'
-        >
-          {/* wrapper */}
+      <div style={{ padding: '3.5%', marginBottom: '10%' }}>
+        <div className='wrapper'>
+          {/* Wrapper Section */}
           <div
             style={{
               flex: 1,
@@ -37,11 +43,9 @@ const Hero = () => {
                 marginLeft: '2%',
                 fontWeight: '300',
                 color: '#808080',
-               
               }}
             >
-              Convert Indian Sign Language
-              <br />
+              Convert Indian Sign Language <br />
               To Text In <br />
               Real-Time With ISHAARA
             </p>
@@ -54,7 +58,9 @@ const Hero = () => {
                 alignItems: 'center',
               }}
             >
-              <Link className="get_tlink" to='/Translate'><button className='get'>Get <span>Started</span></button></Link>
+              <Link className="get_tlink" to='/Translate'>
+                <button className='get'>Get <span>Started</span></button>
+              </Link>
             </div>
           </div>
           <div style={{ flex: 1 }}>
@@ -66,22 +72,22 @@ const Hero = () => {
                 borderTopRightRadius: '100px',
                 marginTop: '-10px',
               }}
-              alt="Your Image"
+              alt="Hero Image"
             />
           </div>
         </div>
       </div>
+      
+      {/* Additional Components */}
       <Counter />
-    <Services/>
-    <Features/>
-    <Usecase />
-    <Try />
-    <Objectives />
-    <Architecture/>
-    <Footer/>
-    {/* <About/> */}
+      <Services />
+      <Features />
+      <Usecase />
+      <Try />
+      <Objectives />
+      <Architecture />
+      <Footer />
     </section>
-
   );
 };
 
